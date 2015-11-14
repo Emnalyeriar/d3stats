15f1d8f600ac3ac998511702a1bea8f4aeaee490
Emnalyeriar <emnalyeriar@gmail.com>
2015-11-14 20:23:21 +0100

    Added function to cummunicate with Battle.net API

---
+++ battlenet/API.py
@@ -0,0 +1,10 @@
+import requests
+
+from django.conf import settings
+
+
+def get_accounts(region, battle_tag):
+
+    url_data = (region, battle_tag, settings.BNET_APIKEY,)
+    r = requests.get("https://{0}.api.battle.net/d3/profile/{1}/?locale=en_GB&apikey={2}".format(*url_data))
+    return r
