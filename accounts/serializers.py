00d93528a446f165b7e245f9a0899e7df2813a84
Mateusz Anioła <emnalyeriar@gmail.com>
2015-11-17 09:55:01 +0000

    serializers.py edited online with Bitbucket

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,10 +10,15 @@ class AccountHistorySerializer(serializers.ModelSerializer):
         exclude = ('account',)
 
 
-class AccountSerializer(serializers.ModelSerializer):
-    history = AccountHistorySerializer(many=True, read_only=True)
+class BaseAccountSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Account
         fields = ('region', 'battle_tag', 'last_updated', 'last_played',
                   'heroes', 'guild_name', 'time_played', 'history')
+
+class AccountSerializer(BaseAccountSerializer):
+    history = AccountHistorySerializer(many=True, read_only=True)
+
+    class Meta(BaseAccountSerializer.Meta):
+        pass
